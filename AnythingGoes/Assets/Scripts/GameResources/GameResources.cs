15f9e0fa17c911e666c2c17704aa67fb4b9f3a7c
StuartMcRoberts <strt.mcroberts@gmail.com>
2019-07-05 19:25:21 +0200

    Added Text Mesh Pro, added start scene

---
+++ AnythingGoes/Assets/Scripts/GameResources/GameResources.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameResources : MonoBehaviour
+{
+    public static GameResources Instance;
+
+    public int CurrentGameLevel { get; internal set; }
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (GameResources.Instance != null)
+        {
+            Debug.Log("Cant have more than one game resources");
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            Instance = this;
+            CurrentGameLevel = 0;
+            if (PlayerPrefs.HasKey("PlayerLevel"))
+            {
+                CurrentGameLevel = PlayerPrefs.GetInt("PlayerLevel");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ AnythingGoes/Assets/Scripts/UI/StartScreenController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartScreenController : MonoBehaviour
+{
+    [SerializeField]
+    Button LoadGameBuuton;
+    public void ClickNewGame()
+    {
+
+    }
+    public void ClickLoadGame()
+    {
+
+    }
+    public void ClickGameCredits()
+    {
+
+    }
+    public void ClickExitGame()
+    {
+        Application.Quit();
+    }
+    private void Start()
+    {
+        if (GameResources.Instance.CurrentGameLevel>0)
+        {
+            LoadGameBuuton.interactable = true;
+        }
+        else
+        {
+            LoadGameBuuton.interactable = false;
+        }
+    }
+
+}
